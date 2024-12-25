86b63a00cb90e9ad2b5fcd907df70b465d4688bc
Tarun Verghis <tarun.verghis@gmail.com>
2024-12-25 00:04:17 -0800

    Rearrange final calculations

--- cmd/cpi-count/main.go
+++ cmd/cpi-count/main.go
@@ -52,16 +52,15 @@ func main() {
 		log.Fatalf("Failed to measure perf group: %s\n", err)
 	}
 
-	cycles, instrs := gc.Values[1].Value, gc.Values[0].Value
-	log.Printf("Output is %s\n", workloadOutput)
-	log.Printf("Ran for %dms\n", gc.Running.Milliseconds())
-	log.Printf("GoPerf Cycles: %d, GoPerf Instrs: %d, GoPerf CPI: %f\n", cycles, instrs, float64(cycles)/float64(instrs))
-
 	perfOutput, err := perfCmd.End()
 	if err != nil {
 		log.Fatalf("Failed to end perf cmd: %v\n", err)
 	}
 
+	cycles, instrs := gc.Values[1].Value, gc.Values[0].Value
+	log.Printf("Output is %s\n", workloadOutput)
+	log.Printf("Ran for %dms\n", gc.Running.Milliseconds())
+	log.Printf("GoPerf Cycles: %d, GoPerf Instrs: %d, GoPerf CPI: %f\n", cycles, instrs, float64(cycles)/float64(instrs))
 	log.Printf("PerfCmd Cycles: %d, PerfCmd Instrs: %d, PerfCmd CPI: %f\n", int64(perfOutput.Cycles), int64(perfOutput.Instrs), perfOutput.Cycles/perfOutput.Instrs)
 }
 
