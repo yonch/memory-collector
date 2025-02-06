3ca9d4565e9948e15ebd7384a0a528c9668eae32
Jonathan Perry <yonch@yonch.com>
2025-02-06 17:40:20 -0600

    remove get_irq_regs(), unused

--- module/memory_collector.c
+++ module/memory_collector.c
@@ -26,8 +26,7 @@ static struct pmu memory_collector_pmu;
 
 // Custom overflow handler
 static void memory_collector_overflow_handler(struct perf_event *event,
-                                           struct perf_sample_data *data,
-                                           struct pt_regs *regs)
+                                           struct perf_sample_data *data)
 {
     struct memory_collector_data sample = {
         .timestamp = ktime_get_ns(),
@@ -66,13 +65,10 @@ static void memory_collector_read(struct perf_event *event)
 {
     // Trigger the overflow handler directly
     struct perf_sample_data data;
-    struct pt_regs *regs = get_irq_regs();
     
     perf_sample_data_init(&data, 0, 1);
     
-    if (regs) {
-        memory_collector_overflow_handler(event, &data, regs);
-    }
+    memory_collector_overflow_handler(event, &data);
 }
 
 // PMU configuration
