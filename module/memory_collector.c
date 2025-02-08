77685ca5972e255e063cff2b53ea6e33d0b7712f
Jonathan Perry <yonch@yonch.com>
2025-02-07 21:55:03 -0600

    add llc miss events

--- module/memory_collector.c
+++ module/memory_collector.c
@@ -20,20 +20,33 @@ MODULE_VERSION("1.0");
 #define CREATE_TRACE_POINTS
 #include "memory_collector_trace.h"
 
-// PMU type for our custom events
-static struct pmu memory_collector_pmu;
+// Keep track of the LLC miss events for each CPU
+static struct perf_event **llc_miss_events;
 
+// Keep track of the time sampling
+static struct perf_event *sampling_event;
 
-// New IPI handler function that will run on each CPU
+// IPI handler function that will run on each CPU
 static void memory_collector_ipi_handler(void *info)
 {
-    u32 cpu = smp_processor_id();
+    u64 timestamp;
+    u32 cpu;
+    u64 llc_misses, enabled, running;
     
-    // Trace the event for this CPU
-    trace_memory_collector_sample(cpu, ktime_get_ns(), current->comm);
+    timestamp = ktime_get_ns();
+    cpu = smp_processor_id();
+    
+    // Read LLC misses
+    if (llc_miss_events[cpu]) {
+        llc_misses = perf_event_read_value(llc_miss_events[cpu], &enabled, &running);
+    } else {
+        llc_misses = 0;
+    }
+
+    trace_memory_collector_sample(timestamp, cpu, current->comm, llc_misses);
 }
 
-// Modified overflow handler
+// Overflow handler for the time sampling event
 static void memory_collector_overflow_handler(struct perf_event *event,
                                            struct perf_sample_data *data,
                                            struct pt_regs *regs)
@@ -48,68 +61,11 @@ static void memory_collector_overflow_handler(struct perf_event *event,
     memory_collector_ipi_handler(NULL);
 }
 
-// PMU callback functions
-static void memory_collector_start(struct perf_event *event, int flags)
-{
-    event->hw.state = 0;
-}
 
-static void memory_collector_stop(struct perf_event *event, int flags)
-{
-    event->hw.state = PERF_HES_STOPPED;
-}
-
-static int memory_collector_add(struct perf_event *event, int flags)
-{
-    return 0;
-}
-
-static void memory_collector_del(struct perf_event *event, int flags)
-{
-}
-
-static void memory_collector_read(struct perf_event *event)
-{
-    // Trigger the overflow handler directly
-    struct perf_sample_data data;
-    struct pt_regs *regs = get_irq_regs();
-    
-    perf_sample_data_init(&data, 0, 1);
-    
-    if (regs) {
-        memory_collector_overflow_handler(event, &data, regs);
-    }
-}
-
-// PMU configuration
-static int memory_collector_event_init(struct perf_event *event)
-{
-    if (event->attr.type != memory_collector_pmu.type)
-        return -ENOENT;
-
-    event->destroy = NULL;
-    event->hw.config = 0;
-    
-    return 0;
-}
-
-// PMU structure
-static struct pmu memory_collector_pmu = {
-    .task_ctx_nr = perf_sw_context,
-    .event_init  = memory_collector_event_init,
-    .add         = memory_collector_add,
-    .del         = memory_collector_del,
-    .start       = memory_collector_start,
-    .stop        = memory_collector_stop,
-    .read        = memory_collector_read,
-};
-
-// Keep track of our event
-static struct perf_event *sampling_event;
-
+// Modify init function:
 static int __init memory_collector_init(void)
 {
-    int ret;
+    int cpu, ret;
     struct perf_event_attr attr = {
         .type = PERF_TYPE_SOFTWARE,
         .size = sizeof(struct perf_event_attr),
@@ -119,12 +75,6 @@ static int __init memory_collector_init(void)
 
     printk(KERN_INFO "Memory Collector: initializing PMU module\n");
 
-    // Register PMU
-    ret = perf_pmu_register(&memory_collector_pmu, "memory_collector", -1);
-    if (ret) {
-        printk(KERN_ERR "Memory Collector: failed to register PMU: %d\n", ret);
-        return ret;
-    }
 
     // Create a kernel counter that will drive our sampling
     sampling_event = perf_event_create_kernel_counter(&attr, 
@@ -135,18 +85,60 @@ static int __init memory_collector_init(void)
     if (IS_ERR(sampling_event)) {
         ret = PTR_ERR(sampling_event);
         printk(KERN_ERR "Memory Collector: failed to create sampling event: %d\n", ret);
-        perf_pmu_unregister(&memory_collector_pmu);
         return ret;
     }
 
     // Enable the event
     perf_event_enable(sampling_event);
 
+    // Allocate array for LLC miss events
+    llc_miss_events = kcalloc(num_possible_cpus(), sizeof(*llc_miss_events), GFP_KERNEL);
+    if (!llc_miss_events) {
+        ret = -ENOMEM;
+        goto error_alloc;
+    }
+
+    // Setup LLC miss event attributes
+    memset(&attr, 0, sizeof(attr));
+    attr.type = PERF_TYPE_HW_CACHE;
+    attr.config = PERF_COUNT_HW_CACHE_MISSES;
+    attr.size = sizeof(attr);
+    attr.disabled = 0;
+    attr.exclude_kernel = 0;
+    attr.exclude_hv = 0;
+    attr.exclude_idle = 0;
+
+    // Create LLC miss counter for each CPU
+    for_each_possible_cpu(cpu) {
+        llc_miss_events[cpu] = perf_event_create_kernel_counter(&attr, cpu, NULL, NULL, NULL);
+        if (IS_ERR(llc_miss_events[cpu])) {
+            pr_err("Failed to create LLC miss event for CPU %d\n", cpu);
+            llc_miss_events[cpu] = NULL;
+            ret = PTR_ERR(llc_miss_events[cpu]);
+            goto error_events;
+        }
+    }
+
     return 0;
+
+error_events:
+    // Cleanup LLC miss events
+    for_each_possible_cpu(cpu) {
+        if (llc_miss_events[cpu]) {
+            perf_event_release_kernel(llc_miss_events[cpu]);
+        }
+    }
+    kfree(llc_miss_events);
+error_alloc:
+    perf_event_disable(sampling_event);
+    perf_event_release_kernel(sampling_event);
+    return ret;
 }
 
 static void __exit memory_collector_exit(void)
 {
+    int cpu;
+    
     printk(KERN_INFO "Memory Collector: unregistering PMU module\n");
     
     if (sampling_event) {
@@ -154,7 +146,14 @@ static void __exit memory_collector_exit(void)
         perf_event_release_kernel(sampling_event);
     }
     
-    perf_pmu_unregister(&memory_collector_pmu);
+
+    // Cleanup LLC miss events
+    for_each_possible_cpu(cpu) {
+        if (llc_miss_events[cpu]) {
+            perf_event_release_kernel(llc_miss_events[cpu]);
+        }
+    }
+    kfree(llc_miss_events);
 }
 
 module_init(memory_collector_init);
--- module/memory_collector_trace.h
+++ module/memory_collector_trace.h
@@ -7,26 +7,29 @@
 #include <linux/tracepoint.h>
 
 TRACE_EVENT(memory_collector_sample,
-    TP_PROTO(u32 core_id, u64 timestamp, const char *comm),
+    TP_PROTO(u32 core_id, u64 timestamp, const char *comm, u64 llc_misses),
     
-    TP_ARGS(core_id, timestamp, comm),
+    TP_ARGS(core_id, timestamp, comm, llc_misses),
     
     TP_STRUCT__entry(
         __field(u32, core_id)
         __field(u64, timestamp)
         __array(char, comm, 16)
+        __field(u64, llc_misses)
     ),
     
     TP_fast_assign(
         __entry->core_id = core_id;
         __entry->timestamp = timestamp;
         memcpy(__entry->comm, comm, 16);
+        __entry->llc_misses = llc_misses;
     ),
     
-    TP_printk("cpu=%u timestamp=%llu comm=%s",
+    TP_printk("cpu=%u timestamp=%llu comm=%s llc_misses=%llu",
         __entry->core_id,
         __entry->timestamp,
-        __entry->comm)
+        __entry->comm,
+        __entry->llc_misses)
 );
 
 #endif /* _MEMORY_COLLECTOR_TRACE_H */
