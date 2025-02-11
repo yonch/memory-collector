faabf6659937cf7f75cfbc407ccf4233ee806dbd
Jonathan Perry <yonch@yonch.com>
2025-02-11 01:31:18 +0000

    loop for 30 seconds, then unload module

--- module/run_module.sh
+++ module/run_module.sh
@@ -12,7 +12,7 @@ start_time=$(date +%s)
 echo "Module loaded, monitoring dmesg output..."
 echo "Press Ctrl+C to stop monitoring"
 
-while true; do
+for i in {1..30}; do
     # Get new dmesg entries and timestamp them
     sudo dmesg -c | while read line; do
         current_time=$(($(date +%s) - start_time))
@@ -21,3 +21,5 @@ while true; do
     
     sleep 1
 done 
+
+sudo rmmod collector
