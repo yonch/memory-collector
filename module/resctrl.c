170862fa4c1e45a24059650e500addfe130395bf
Jonathan Perry <yonch@yonch.com>
2025-02-18 16:48:21 +0000

    remove early testing code in cpuid enumeration

--- module/resctrl.c
+++ module/resctrl.c
@@ -112,15 +112,6 @@ int resctrl_init_cpu(struct rdt_state *rdt_state)
     pr_debug(LOG_PREFIX "capabilities of core %d: llc_occupancy: %d, mbm_total: %d, mbm_local: %d, max_rmid: %d, counter_width: %d, has_overflow_bit: %d, supports_non_cpu_agent_cache: %d, supports_non_cpu_agent_mbm: %d\n", 
              cpu, rdt_state->supports_llc_occupancy, rdt_state->supports_mbm_total, rdt_state->supports_mbm_local, rdt_state->max_rmid, rdt_state->counter_width, rdt_state->has_overflow_bit, rdt_state->supports_non_cpu_agent_cache, rdt_state->supports_non_cpu_agent_mbm);
 
-    if (cpu == 2) {
-        u32 rmid = 1;
-        u32 closid = 0;
-        if (write_rmid_closid(rmid, closid) != 0) {
-            pr_err(LOG_PREFIX "failed to write RMID %d to MSR_IA32_PQR_ASSOC\n", rmid);
-        } else {
-            pr_info(LOG_PREFIX "wrote RMID %d to MSR_IA32_PQR_ASSOC\n", rmid);
-        }
-    }    
 
     pr_debug(LOG_PREFIX "enumerate_cpuid completed successfully on CPU %d\n", cpu);
     return ret;
