7b82edde62569454c88940268e5fa6507bd13b4f
Jonathan Perry <yonch@yonch.com>
2025-02-22 22:07:57 +0000

    change generated prefix taskCounter -> bpf

--- cmd/collector/gen.go
+++ cmd/collector/gen.go
@@ -1,3 +1,3 @@
 package main
 
-//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -target bpfel -cc clang -type msg_type -type event -type rmid_alloc_msg -type rmid_free_msg taskCounter task_counter.c
+//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -target bpfel -cc clang -type msg_type -type event -type rmid_alloc_msg -type rmid_free_msg bpf task_counter.c
