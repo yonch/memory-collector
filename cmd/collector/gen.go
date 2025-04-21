76e80c34c4aa6e443714f2534e012b028603ef96
Jonathan Perry <yonch@yonch.com>
2025-04-21 22:20:39 +0000

    move generate line into main.go

--- cmd/collector/gen.go
+++
@@ -1,3 +0,0 @@
-package main
-
-//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -target bpfel -cc clang -type msg_type -type perf_measurement_msg -type rmid_alloc_msg -type rmid_free_msg bpf collector.c protocol.bpf.c
